08df01ab1db7db5a960e0bfb01417109cb8a5ba7
19WH1A0578 <69664117+19WH1A0578@users.noreply.github.com>
2020-08-17 07:45:53 -0700

    Add files via upload

---
+++ weel 1/student info.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+struct  student
+{
+                    char name[30];
+                    int rollno;
+                    int age;
+
+]s[10];
+int main()
+{
+                   int i, n;
+                   printf("Enter no. of students:");
+                   scanf("%d",&n);
+                   for(i = 0; i < n; i ++){
+             
+                   printf("Enter name of student:");
+                   scanf("%s",&s[i].name);
+   
+                   printf("Enter roll no.:");
+                   scanf("%d",&s[i].rollno);
+ 
+                   printf(Enter age:");
+                   scanf("%d",&s[i].marks);
+                   }
+                   for (int i = 0; i< n; i++)
+{
+                   printf("student information: \n");
+                   printf("name:");
+                   puts(s[i].name);
+                   printf("/nroll no : %d",s[i].rollno):
+                   printf("\nage : %d",s[i].age);
+                  }
+                   return 0;
+}
